eaee4c4dc25416aa41e26fffc7b483df1350cdf5
Steve McQuaid <steve@stevemcquaid.com>
2017-03-13 10:27:44 -0700

    DBus debugging Current error message: `dbus.exceptions.DBusException: org.freedesktop.DBus.Error.ServiceUnknown: The name org.asamk.Signal was not provided by any .service files`

--- docker/weechat.sh
+++ docker/weechat.sh
@@ -4,8 +4,8 @@
 docker build . -t signal-weechat
 
 # guest
-docker run -v "$(pwd)"/.weechat:/home/guest/.weechat -v "$(dirname $(pwd))":/signal-weechat -it signal-weechat:latest
+# docker run -v "$(pwd)"/.weechat:/home/guest/.weechat -v "$(dirname $(pwd))":/signal-weechat -it signal-weechat:latest
 
 # root
-# docker run -v "$(pwd)"/.weechat:/root/.weechat -v "$(dirname $(pwd))":/signal-weechat -it signal-weechat:latest
+docker run -v "$(pwd)"/.weechat:/root/.weechat -v "$(dirname $(pwd))":/signal-weechat -it signal-weechat:latest
 
